d87f57d8ecea79dbe28fa188dcbc1629e4fb70e1
Ibrokhimkhuja_Makhmudov <Ibrokhimkhuja_Makhmudov@epam.com>
2022-03-20 20:45:51 +0500

    add test tables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firts-app';
+  programsSettings = {
+    colHeaders: ['время запуска', 'время вывполнения', 'приоритет'],
+    rowHeaders(index) {
+      return 'P' + (index + 1);
+    },
+    height: 'auto',
+    licenseKey: 'non-commercial-and-evaluation'
+  }
+
+  tableSettings = {
+    colHeaders(index) {
+      return `${index}`;
+    },
+    rowHeaders(index) {
+      return 'P' + (index + 1);
+    },
+    height: 'auto',
+    editor: false,
+    licenseKey: 'non-commercial-and-evaluation'
+  }
 }
